0dc02b0e0075b4bf2afe67957ab01b920e3a2b00
Amar Mandal <amarmandal2073@gmail.com>
2021-05-22 10:11:54 +0545

    Feat: User can now review the place

--- backend/controllers/placeController.js
+++ backend/controllers/placeController.js
@@ -1,64 +1,68 @@
-const Place = require('../models/placeModel');
+const Place = require("../models/placeModel");
 
 exports.getPlaceById = async (req, res, next, id) => {
-    try {
-        const place = await Place.findById({_id: id.toString()});
-        req.place = place;
-        next();
-    } catch (error) {
-        res.status(200).json({error: "Cannot find the place associated with Id"})
-        return;
-    }
-}
+  try {
+    const place = await Place.findById({_id: id})
+      .populate({ path: "reviews", select: "userId -_id" })
+      .exec();
+    req.place = place;
+    next();
+  } catch (error) {
+    res.status(200).json({ error: "Cannot find the place associated with Id" });
+    return;
+  }
+};
 
 // exports.getPlaceByLocation = () => {
 //     //
 // }
 
 exports.createPlace = async (req, res) => {
-    try {
-       const place = new Place(req.body);
-       await place.save();
-       return res.status(200).json(place);
-    } catch (error) {
-        res.status(400).json({error: 'Cannot save place in db'})
-        return;
-    }
-}
+  try {
+    const place = new Place(req.body);
+    await place.save();
+    return res.status(200).json(place);
+  } catch (error) {
+    res.status(400).json({ error: "Cannot save place in db" });
+    return;
+  }
+};
 
-exports.updatePlace = async(req, res) => {
-    const { name, placePhoto, category } = req.body;
-    const place = req.place;
-    try {
-        place.name = name;
-        place.placePhoto = placePhoto;
-        place.category = category;
+exports.updatePlace = async (req, res) => {
+  const { name, placePhoto, category } = req.body;
+  const place = req.place;
+  try {
+    place.name = name;
+    place.placePhoto = placePhoto;
+    place.category = category;
 
-        await place.save();
-        return res.status(200).json({ data: place});
-     } catch (error) {
-         console.log(error);
-         res.status(400).json({error: 'Cannot save place in db'})
-         return;
-     }
-}
+    await place.save();
+    return res.status(200).json({ data: place });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Cannot save place in db" });
+    return;
+  }
+};
 
 exports.getAllPlace = async (req, res) => {
-    try {
-        const places = await Place.find({});
-        return res.status(200).json(places);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json({ error: 'Error fetching places from DB'});
-    }
-}
+  try {
+    const places = await Place.find({})
+      .populate({ path: "reviews", select: "userId reviewText rating -_id" })
+      .exec();
+    return res.status(200).json(places);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: "Error fetching places from DB" });
+  }
+};
 
-exports.deletePlace = async(req, res) => {
-    try {
-        const deletedPlace = await Place.findByIdAndDelete({_id: req.place._id})
-        return res.status(200).json({ deletedPlace });
-     } catch (error) {
-         res.status(400).json({error: 'Cannot delete place from db'})
-         return;
-     }
-}
+exports.deletePlace = async (req, res) => {
+  try {
+    const deletedPlace = await Place.findByIdAndDelete({ _id: req.place._id });
+    return res.status(200).json({ deletedPlace });
+  } catch (error) {
+    res.status(400).json({ error: "Cannot delete place from db" });
+    return;
+  }
+};
--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const { createPlaceReview } = require('../controllers/reviewController');
+const { getPlaceById } = require('../controllers/placeController');
 const { isSignedIn, isAuthenticated } = require('../middleware/authMiddleware');
 
+router.param('placeId', getPlaceById);
 
 router.post('/review/create/:placeId', isSignedIn, isAuthenticated, createPlaceReview);
 
