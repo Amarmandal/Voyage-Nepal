1b705b5737190df859889caeb7a19b06582bf793
Amar Mandal <amarmandal2073@gmail.com>
2021-06-06 08:28:10 +0545

    Fix: Get desired user details

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,18 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { uploadPhoto, updatePhoto, getUserById, removeUserById, updateUserRole } = require('../controllers/userController');
-const { isSignedIn, isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
-const { upload } = require('../utils/uploadHelper');
+const {
+  uploadPhoto,
+  updatePhoto,
+  getUserById,
+  removeUserById,
+  updateUserRole,
+  getUserDetails,
+} = require("../controllers/userController");
+const {
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+} = require("../middleware/authMiddleware");
+const { upload } = require("../utils/uploadHelper");
 
-router.param('userId', getUserById);
+router.param("userId", getUserById);
 
-router.post('/upload/photo/:userId', isSignedIn, isAuthenticated, upload.single('photo'), uploadPhoto);
+router.post(
+  "/upload/photo/:userId",
+  isSignedIn,
+  isAuthenticated,
+  upload.single("photo"),
+  uploadPhoto
+);
 
-router.put('/update/photo/:userId', isSignedIn, isAuthenticated, upload.single('photo'), updatePhoto);
+router.get(
+  "/user/user-details/:userId",
+  isSignedIn,
+  isAuthenticated,
+  getUserDetails
+);
 
-router.delete('/user/:userId', isSignedIn, isAuthenticated, isAdmin, removeUserById);
+router.put(
+  "/update/photo/:userId",
+  isSignedIn,
+  isAuthenticated,
+  upload.single("photo"),
+  updatePhoto
+);
 
-router.put('/user/update-role/:userId', isSignedIn, isAuthenticated, isAdmin, updateUserRole);
+router.delete(
+  "/user/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  removeUserById
+);
+
+router.put(
+  "/user/update-role/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateUserRole
+);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,10 @@ mongoose.connection
 
 //all middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+	origin: "http://localhost:3000",
+	credentials: true
+}));
 app.use(cookieParser());
 
 //all routes
