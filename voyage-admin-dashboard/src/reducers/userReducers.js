480175597c6fd31ca6d9b10d4d2aed1bbb933771
Amar Mandal <amarmandal2073@gmail.com>
2021-06-21 17:26:52 +0545

    Fix: Email Verification bug fixed

--- voyage-admin-dashboard/src/reducers/userReducers.js
+++ voyage-admin-dashboard/src/reducers/userReducers.js
@@ -2,7 +2,11 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
-  USER_LOGOUT
+  USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_RESET,
+  USER_REGISTER_SUCCESS
 } from "../actions/action.types";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -12,10 +16,24 @@ export const userLoginReducer = (state = {}, action) => {
     case USER_LOGIN_SUCCESS:
       return { userInfo: action.payload, loading: false };
     case USER_LOGIN_FAIL:
-      console.log('logout_fail')
       return {loading: false, error: action.payload};
     case USER_LOGOUT: 
       return { }
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true, registerInfo: {} };
+    case USER_REGISTER_SUCCESS:
+      return { registerInfo: action.payload, loading: false, success: true };
+    case USER_REGISTER_FAIL:
+      return {loading: false, error: action.payload};
+    case USER_REGISTER_RESET: 
+      return {}
     default:
       return state;
   }
