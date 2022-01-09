4f49c48e111589c3803b4e244ea0e834cd88ce73
Amar Mandal <amarmandal2073@gmail.com>
2022-01-09 21:12:40 +0545

    some changes

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,50 @@
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(express.static("public"));
+
 //connecting database mongoose
 mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 mongoose.connection
-    .once('open', () => {
-        console.log('DATABASE CONNECTED SUCCESSFULLY');
-    })
-    .on('error', err => console.log(err))
+  .once("open", () => {
+    console.log("DATABASE CONNECTED SUCCESSFULLY");
+  })
+  .on("error", (err) => console.log(err));
 
 //all middlewares
 app.use(express.json());
 app.use(cors());
 
 //all routes
-const userRoute = require('./routes/userRoutes');
-const authRoute = require('./routes/authRoutes');
-const categoryRoute = require('./routes/categoryRoutes');
-const reviewRoute = require('./routes/reviewRoutes');
-const placeRoute = require('./routes/placeRoutes');
-const hotresRoute = require('./routes/hotresRoutes');
-const coreRoute = require('./routes/coreRoute');
+const userRoute = require("./routes/userRoutes");
+const authRoute = require("./routes/authRoutes");
+const categoryRoute = require("./routes/categoryRoutes");
+const reviewRoute = require("./routes/reviewRoutes");
+const placeRoute = require("./routes/placeRoutes");
+const hotresRoute = require("./routes/hotresRoutes");
+const coreRoute = require("./routes/coreRoute");
 
-app.use('/api', userRoute);
-app.use('/api', categoryRoute);
-app.use('/api', authRoute);
-app.use('/api', reviewRoute);
-app.use('/api', placeRoute);
-app.use('/api', hotresRoute);
-app.use('/api', coreRoute);
-
+app.use("/api", userRoute);
+app.use("/api", categoryRoute);
+app.use("/api", authRoute);
+app.use("/api", reviewRoute);
+app.use("/api", placeRoute);
+app.use("/api", hotresRoute);
+app.use("/api", coreRoute);
 
 app.listen(port, (err) => {
-    if(!err) {
-        console.log(`App listening at ${port}`);
-    } else  {
-        console.log(err);
-    }
-})
-
-
+  if (!err) {
+    console.log(`App listening at ${port}`);
+  } else {
+    console.log(err);
+  }
+});
