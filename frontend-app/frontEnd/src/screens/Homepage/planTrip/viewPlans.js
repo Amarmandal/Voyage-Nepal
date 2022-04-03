34b07ff99dcaae745d15ced4ff399a24989c7f14
Aarati Shah <aaratishah2057@gmail.com>
2022-04-04 00:54:42 +0545

    View Plans Page

--- frontend-app/frontEnd/src/screens/Homepage/planTrip/viewPlans.js
+++ frontend-app/frontEnd/src/screens/Homepage/planTrip/viewPlans.js
@@ -5,46 +5,82 @@ import {
   ImageBackground,
   StyleSheet,
   useWindowDimensions,
+  Image,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Content, Button, Container, Icon} from 'native-base';
+import PlaceIcon from 'react-native-vector-icons/MaterialIcons';
 import PlusIcon from 'react-native-vector-icons/AntDesign';
 import Colors from '../../../constants/Color';
+import PlaceList from '../../../Components/Home/PlanTrip/PlaceList';
 
 const ViewPlans = props => {
-  const imageWidth = useWindowDimensions().width;
   return (
-    <View>
-      <View style = {{display: 'flex', justifyContent: 'space-between', flexDirection: 'column', alignItems: 'center'}}>
-        <Text style={{fontSize: 22, fontWeight: 'bold'}}>My Plans</Text>
-        <PlusIcon name="pluscircle" color="#000" size={28}></PlusIcon>
-      </View>
-      <ImageBackground
-        style={[style.headerImage, {width: imageWidth}]}
-        source={require('./baudhanathStupa.jpg')}>
-        <View style={style.header}>
-          <View style={{marginTop: 20}}>
-            <Text style={{fontSize: 20, fontWeight: 'bold', color: '#000'}}>
-              Place Name
-            </Text>
-            <View
-              style={{
-                flexDirection: 'row',
-              }}>
-              <Icon name="place" color="#000" size={28} />
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontWeight: '400',
-                  color: '#000',
-                  marginTop: 5,
-                }}>
-                Kathmandu
-              </Text>
-            </View>
-          </View>
+    <Container>
+      <Content padder style = {{marginBottom: 40}}>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            margin: 20,
+          }}>
+          <Text style={{fontSize: 22, fontWeight: 'bold'}}>My Plans</Text>
+          <Icon
+            type="AntDesign"
+            name="pluscircle"
+            color="#000"
+            size={28}></Icon>
         </View>
-      </ImageBackground>
-    </View>
+        <Image
+          style={style.headerImage}
+          source={require('./baudhanathStupa.jpg')}
+        />
+        <View style={style.header}>
+          <Text style={{fontSize: 20, fontWeight: 'bold', color: '#000'}}>
+            Place Name
+          </Text>
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: 'bold',
+              color: '#000',
+              paddingTop: 5,
+            }}>
+            25 Mar - 30 Mar
+          </Text>
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: 'bold',
+              color: Colors.warning,
+              paddingTop: 5,
+            }}>
+            View all Places
+          </Text>
+          {/* <View
+            style={{
+              flexDirection: 'row',
+            }}>
+            <PlaceIcon name="place" color="#000" size={28} />
+            <Text
+              style={{
+                fontSize: 12,
+                fontWeight: '400',
+                color: '#000',
+                marginTop: 5,
+              }}>
+              Kathmandu
+            </Text>
+          </View> */}
+        </View>
+        <View style = {{marginTop: 70}}>
+          <PlaceList />
+          <PlaceList />
+          <PlaceList />
+        </View>
+      </Content>
+    </Container>
   );
 };
 
@@ -53,13 +89,21 @@ export default ViewPlans;
 const style = StyleSheet.create({
   headerImage: {
     height: 250,
-    borderBottomRightRadius: 15,
-    borderBottomLeftRadius: 15,
-    overflow: 'hidden',
+    borderRadius: 15,
+    // overflow: 'hidden',
+    width: '100%',
+    position: 'relative'
   },
   header: {
-    flexDirection: 'row',
-    alignItems: 'center',
     marginHorizontal: 10,
+    position: 'absolute',
+    backgroundColor: '#fff',
+    width: '85%',
+    elevation: 5,
+    borderRadius: 20,
+    padding: 20,
+    top: 260,
+    left: '8%',
+    right: '8%',
   },
 });
