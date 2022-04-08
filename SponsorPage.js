31a7af98ecd047f31b7f396b8398c7d12e98d0a5
Neha Verma <47393193+NehaVerma14@users.noreply.github.com>
2022-04-08 08:23:49 +0545

    Add files via upload

--- SponsorPage.js
+++ SponsorPage.js
@@ -1,4 +1,4 @@
-import {View, Text, SafeAreaView, Pressable} from 'react-native';
+import {View, Text, SafeAreaView, Pressable, FlatList} from 'react-native';
 import Colors from '../../../../constants/Color';
 
 import {Item, Input, Icon} from 'native-base';
@@ -32,33 +32,85 @@ const SponsorPage = ({navigation}) => {
           Your Advertisements
         </Text>
       </View>
-      <Pressable
-        onPress={() => {
-          navigation.navigate('banner');
-        }}>
-        <View
-          style={{
-            width: '45%',
-            height: '20%',
-            marginLeft: 20,
-            backgroundColor: Colors.themeColor,
-            flexDirection: 'row',
-            justifyContent: 'center',
-          }}>
-          <Icon type="FontAwesome5" name="calendar-alt" style={{margin: 45}} />
-        </View>
-      </Pressable>
+
       <View
         style={{
-          width: '45%',
-          height: '20%',
-          marginLeft: 20,
-          marginTop: 20,
-          backgroundColor: Colors.themeColor,
+          width: '100%',
+          height: '85%',
           flexDirection: 'row',
-          justifyContent: 'center',
+          flexWrap: 'wrap',
+          marginLeft: 25,
+          marginRight: 25,
         }}>
-        <Icon type="FontAwesome5" name="calendar-alt" style={{margin: 45}} />
+        <View style={{width: '45%', height: '25%', padding: 8}}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: Colors.themeColor,
+              alignItems: 'center',
+            }}>
+            <Icon
+              type="Ionicons"
+              name="calendar-sharp"
+              style={{color: Colors.white, fontSize: 70, marginTop: 25}}
+              onPress={() => navigation.navigate('banner')}
+            />
+            <Text style={{color: Colors.white, fontWeight: 'bold'}}>
+              Event Scheduler
+            </Text>
+          </View>
+        </View>
+        <View style={{width: '45%', height: '25%', padding: 8}}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: Colors.themeColor,
+              alignItems: 'center',
+            }}>
+            <Icon
+              type="Ionicons"
+              name="checkmark-circle-sharp"
+              style={{color: Colors.white, fontSize: 70, marginTop: 25}}
+            />
+            <Text style={{color: Colors.white, fontWeight: 'bold'}}>
+              Ads Posted
+            </Text>
+          </View>
+        </View>
+        <View style={{width: '45%', height: '25%', padding: 8}}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: Colors.themeColor,
+              alignItems: 'center',
+            }}>
+            <Icon
+              type="Ionicons"
+              name="people-sharp"
+              style={{color: Colors.white, fontSize: 70, marginTop: 25}}
+            />
+            <Text style={{color: Colors.white, fontWeight: 'bold'}}>
+              Feedback
+            </Text>
+          </View>
+        </View>
+        <View style={{width: '45%', height: '25%', padding: 8}}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: Colors.themeColor,
+              alignItems: 'center',
+            }}>
+            <Icon
+              type="Ionicons"
+              name="checkmark-circle-sharp"
+              style={{color: Colors.white, fontSize: 70, marginTop: 25}}
+            />
+            <Text style={{color: Colors.white, fontWeight: 'bold'}}>
+              Others
+            </Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
