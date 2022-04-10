8f966a2d7ff412e347aae1fca28fba379c9d9a23
Amar Mandal <amarmandal2073@gmail.com>
2022-04-10 09:27:51 +0545

    feat: bookmark and trip plan model

---
+++ backend/models/bookmarkModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const bookmarkSchema = new Schema(
+	{
+		userId: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			unique: true,
+			trim: true,
+			ref: 'User',
+		},
+		placeId: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			unique: true,
+			trim: true,
+			ref: 'Place',
+		},
+	},
+	{ timestamps: true }
+)
+
+const Bookmark = mongoose.model('Bookmark', bookmarkSchema)
+
+module.exports = Bookmark
