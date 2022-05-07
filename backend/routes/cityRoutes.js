7afa0f7e625e3c1f2cdd4324271f399fae70712d
Amar Mandal <amarmandal2073@gmail.com>
2022-05-08 00:28:31 +0545

    feat: get place by city

--- backend/routes/cityRoutes.js
+++ backend/routes/cityRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 //controllers
-const { createNewCity } = require('../controllers/cityController')
+const { createNewCity, getAllCities, getPlaceByCity } = require('../controllers/cityController')
 const { isSignedIn, getUserProfile, isAdmin } = require('../middleware/authMiddleware')
 
 // router.param('categoryId', getCategoryById)
@@ -43,5 +43,11 @@ const { isSignedIn, getUserProfile, isAdmin } = require('../middleware/authMiddl
 
 //create a New City
 router.post('/city/create', isSignedIn, getUserProfile, isAdmin, createNewCity)
+
+//Get all cities
+router.get('/city', isSignedIn, getAllCities)
+
+//Get places by city
+router.get('/city/:cityId', isSignedIn, getPlaceByCity)
 
 module.exports = router
